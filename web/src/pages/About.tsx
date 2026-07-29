e54d708ec79dfa3535f9994a42783a94de28b17f
Chase Wright <chase.wright@ethereum.org>
2026-07-28 20:34:24 -0500

    bump web dependencies and migrate to react-router 8 (#31)
    * bump web dependencies and migrate to react-router 8
    
    * wait for the sort header to re-render before asserting

--- web/src/pages/About.tsx
+++ web/src/pages/About.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom";
+import { useLocation } from "react-router";
 import { fetchMeta } from "../api";
 
 const DEFAULT_SOURCE_URL = "https://github.com/MysticRyuujin/enrscout";
